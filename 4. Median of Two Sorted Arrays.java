52e81fcd6bead4f8b44e570ccdb3d9cc8b70a1d4
tusharjadwani <117591585+tusharjadwani@users.noreply.github.com>
2026-06-21 23:01:09 +0530

    Updating LeetCode Solution: Median of Two Sorted Arrays

--- 4. Median of Two Sorted Arrays.java
+++ 4. Median of Two Sorted Arrays.java
@@ -1,35 +1,40 @@
 class Solution {
-    
-    int x=0,y=0;int no=0;
-
-    private int getNextMin(int[] a, int[] b){
-            if(x==a.length){
-               return b[y++];
-            }
-            else if(y==b.length){
-                return a[x++];
-            }
-            else if(a[x]<=b[y]) {
-                return a[x++];
-            }
-            else{
-               return b[y++];
-            }
-    }
 
     public double findMedianSortedArrays(int[] a, int[] b) {
-        int len = a.length+b.length; 
-        if(len % 2 != 0){
-            for(int i=0;i<len/2;i++) {
-                getNextMin(a,b);
-            }
-            return getNextMin(a,b);  
+        if(a.length>b.length){
+           return findMedianSortedArrays(b,a);
+        }
+        int low = 0,high = a.length;
+        int m = a.length,n=b.length; 
+
+        int i = (low+high)/2;
+        int j = (m+n+1)/2-i;
+
+        int maxLA= (i==0)? Integer.MIN_VALUE: a[i-1];
+        int minRA= (i==m) ? Integer.MAX_VALUE:a[i];
+        int maxLB=(j==0)? Integer.MIN_VALUE: b[j-1];
+        int minRB=(j==n) ? Integer.MAX_VALUE:b[j];
+
+        while(low<=high) {
+            if(maxLA>minRB)
+                high = i-1;
+            else
+                low = i+1;
+
+            i = (low+high)/2;
+            j = (m+n+1)/2-i;
+
+         maxLA= (i==0)? Integer.MIN_VALUE: a[i-1];
+         minRA= (i==m) ? Integer.MAX_VALUE:a[i];
+         maxLB=(j==0)? Integer.MIN_VALUE: b[j-1];
+         minRB=(j==n) ? Integer.MAX_VALUE:b[j];
+        }
+        
+        if((m+n) % 2 != 0){
+            return Math.max(maxLA,maxLB);  
         }
         else{
-            for(int i=0;i<len/2-1;i++) {
-                getNextMin(a,b);
-            }
-            return ((double) getNextMin(a,b) + getNextMin(a,b))/2;
+            return ((double) Math.max(maxLA,maxLB)+Math.min(minRA,minRB))/2;
         }
 
     }
