857d8bf6d904171c37c2a91013ae07f59d5e01ba
tusharjadwani <117591585+tusharjadwani@users.noreply.github.com>
2026-06-21 23:55:55 +0530

    Updating LeetCode Solution: Median of Two Sorted Arrays

--- 4. Median of Two Sorted Arrays.java
+++ 4. Median of Two Sorted Arrays.java
@@ -1,42 +1,36 @@
 class Solution {
-
     public double findMedianSortedArrays(int[] a, int[] b) {
-
-        if(a.length>b.length){
-           return findMedianSortedArrays(b,a);
+        if (a.length > b.length) {
+            return findMedianSortedArrays(b, a);
         }
-        int low = 0,high = a.length;
-        int m = a.length,n=b.length; 
 
-        int i = (low+high)/2;
-        int j = (m+n+1)/2-i;
+        int m = a.length;
+        int n = b.length;
+        int low = 0, high = m;
 
-        int maxLA= (i==0)? Integer.MIN_VALUE: a[i-1];
-        int minRA= (i==m) ? Integer.MAX_VALUE:a[i];
-        int maxLB=(j==0)? Integer.MIN_VALUE: b[j-1];
-        int minRB=(j==n) ? Integer.MAX_VALUE:b[j];
+        while (low <= high) {
+            int i = (low + high) / 2;
+            int j = (m + n + 1) / 2 - i;
 
-        while(low<=high) {
-            if(maxLA>minRB)
-                high = i-1;
-            else
-                low = i+1;
+            int maxLA = (i == 0) ? Integer.MIN_VALUE : a[i - 1];
+            int minRA = (i == m) ? Integer.MAX_VALUE : a[i];
+            int maxLB = (j == 0) ? Integer.MIN_VALUE : b[j - 1];
+            int minRB = (j == n) ? Integer.MAX_VALUE : b[j];
 
-            i = (low+high)/2;
-            j = (m+n+1)/2-i;
-
-         maxLA= (i==0)? Integer.MIN_VALUE: a[i-1];
-         minRA= (i==m) ? Integer.MAX_VALUE:a[i];
-         maxLB=(j==0)? Integer.MIN_VALUE: b[j-1];
-         minRB=(j==n) ? Integer.MAX_VALUE:b[j];
+            if (maxLA <= minRB && maxLB <= minRA) {
+                if ((m + n) % 2 != 0) {
+                    return Math.max(maxLA, maxLB);
+                } else {
+                    return ((double) Math.max(maxLA, maxLB) + Math.min(minRA, minRB)) / 2.0;
+                }
+            } 
+            else if (maxLA > minRB) {
+                high = i - 1;
+            } else {
+                low = i + 1;
+            }
         }
         
-        if((m+n) % 2 != 0){
-            return Math.max(maxLA,maxLB);  
-        }
-        else{
-            return ((double) Math.max(maxLA,maxLB)+Math.min(minRA,minRB))/2;
-        }
-
+        return 0;
     }
 }
