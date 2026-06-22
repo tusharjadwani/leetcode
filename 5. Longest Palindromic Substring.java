e5cfd7a12dc5fd798ee23749a487817c8b5e3962
tusharjadwani <117591585+tusharjadwani@users.noreply.github.com>
2026-06-22 18:11:49 +0530

    Adding LeetCode Solution: Longest Palindromic Substring

---
+++ 5. Longest Palindromic Substring.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public String longestPalindrome(String s) {
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = findLongestPalindrome(s, i, i);
+            int len2 = findLongestPalindrome(s, i, i + 1);
+            int max = Math.max(len1, len2);
+            if (max > end - start) {
+                start = i - (max - 1) / 2;
+                end = i + max / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int findLongestPalindrome(String s, int left, int right) {
+        while (left >= 0 && right < s.length()) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left--;
+                right++;
+            } else
+                break;
+        }
+        return right - left - 1;
+    }
+
+}
