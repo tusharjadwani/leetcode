bdc0064ec5ca092cd9f39247fcf0d13a13e98e6e
tusharjadwani <117591585+tusharjadwani@users.noreply.github.com>
2026-06-22 22:06:02 +0530

    Adding LeetCode Solution: Zigzag Conversion

---
+++ 6. Zigzag Conversion.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        
+        if(numRows ==1)
+            return s;
+
+        boolean topDown = false;
+        StringBuilder[] stbArr = new StringBuilder[numRows];
+
+        for (int i = 0; i < numRows; i++) {
+            stbArr[i] = new StringBuilder();
+        }
+
+        int row = 0;
+        for (char c : s.toCharArray()) {
+            stbArr[row].append(c);
+
+            if (row == numRows - 1 || row == 0)
+                topDown = !topDown;
+
+            row += topDown ? 1 : -1;
+        }
+        StringBuilder res = new StringBuilder();
+
+        for (StringBuilder stb : stbArr) {
+            res.append(stb);
+        }
+        return res.toString();
+    }
+}
