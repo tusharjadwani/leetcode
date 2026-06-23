f1eb1bdf068761257c4f43add75000bd81d83340
tusharjadwani <117591585+tusharjadwani@users.noreply.github.com>
2026-06-23 15:23:12 +0530

    Updating LeetCode Solution: Reverse Integer

--- 7. Reverse Integer.java
+++ 7. Reverse Integer.java
@@ -3,7 +3,7 @@ class Solution {
         int rev = 0;
         while (x != 0) {
 
-            if (rev > (Math.pow(2, 31) - 1) / 10 || rev < (-1 * Math.pow(2, 31)) / 10)
+            if (rev > Integer.MAX_VALUE / 10 || rev < (-1 * Integer.MIN_VALUE) / 10)
                 return 0;
 
             rev = 10 * rev + x % 10;
