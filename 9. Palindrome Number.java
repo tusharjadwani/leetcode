a9b1b9adbb9e976a33eb3b92b4649b18bde61e32
tusharjadwani <117591585+tusharjadwani@users.noreply.github.com>
2026-06-24 18:56:57 +0530

    Adding LeetCode Solution: Palindrome Number

---
+++ 9. Palindrome Number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        if(x<0)
+        return false;
+        
+        int no=x;
+        int rev =0;
+        while(x>0) {
+            rev=rev*10+x%10;
+            x/=10;
+        }
+        if(no==rev)
+        return true;
+        
+        return false;
+        
+    }
+}
