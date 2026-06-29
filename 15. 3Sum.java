ae5305baea29cc13d534429320c99555014e1a41
Tushar Jadwani <117591585+tusharjadwani@users.noreply.github.com>
2026-06-29 20:21:35 +0530

    Updating LeetCode Solution: 3Sum

--- 15. 3Sum.java
+++ 15. 3Sum.java
@@ -24,9 +24,6 @@ class Solution {
                     while (left < right && nums[left] == nums[left + 1])
                         left++;
 
-                    while (left < right && nums[right] == nums[right - 1])
-                        right--;
-
                     left++;
                     right--;
                 }
