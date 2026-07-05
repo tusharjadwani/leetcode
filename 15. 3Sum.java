00afde3f48859b6fb26cd0517c393fdd4cd1d089
Tushar Jadwani <117591585+tusharjadwani@users.noreply.github.com>
2026-07-05 12:58:10 +0530

    Updating LeetCode Solution: 3Sum

--- 15. 3Sum.java
+++ 15. 3Sum.java
@@ -4,8 +4,9 @@ class Solution {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
         for (int i = 0; i < len; i++) {
-            if (nums[i] > 0)
+            if (nums[i] > 0) {
                 break;
+            }
 
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
