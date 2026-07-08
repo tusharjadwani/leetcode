ce04b2896b42eefcdb7ce58da98923c0f6a09c43
Tushar Jadwani <117591585+tusharjadwani@users.noreply.github.com>
2026-07-09 00:02:52 +0530

    Updating LeetCode Solution: 4Sum

--- 18. 4Sum.java
+++ 18. 4Sum.java
@@ -9,12 +9,22 @@ class Solution {
         Arrays.sort(nums);
 
         int len = nums.length;
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < len - 3; i++) {
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
 
-            for (int j = i + 1; j < len; j++) {
+            if ((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target)
+                break;
+            if ((long) nums[i] + nums[len - 3] + nums[len - 2] + nums[len - 1] < target)
+                continue;
+
+            for (int j = i + 1; j < len - 2; j++) {
                 if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+
+                if ((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target)
+                    break;
+                if ((long) nums[i] + nums[j] + nums[len - 2] + nums[len - 1] < target)
                     continue;
 
                 int left = j + 1;
