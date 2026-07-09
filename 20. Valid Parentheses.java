258d3a6f5972d7d9c00d8eb082e08c243090b230
Tushar Jadwani <117591585+tusharjadwani@users.noreply.github.com>
2026-07-10 00:00:52 +0530

    Updating LeetCode Solution: Valid Parentheses

--- 20. Valid Parentheses.java
+++ 20. Valid Parentheses.java
@@ -1,6 +1,11 @@
 class Solution {
     public boolean isValid(String c) {
 
+        if (c.length() % 2 == 1)
+            return false;
+        if (c.charAt(0) == ')' || c.charAt(0) == '}' || c.charAt(0) == ']')
+            return false;
+
         char[] arr = new char[c.length()];
         int i = 0;
         for (char s : c.toCharArray()) {
@@ -9,7 +14,7 @@ class Solution {
             } else if (i > 0 && ((s == '}' && arr[i - 1] == '{') || (s == ')' && arr[i - 1] == '(')
                     || (s == ']' && arr[i - 1] == '['))) {
                 i = i - 2;
-            } else if(s == '}' || s == ')' || s == ']'){
+            } else if (s == '}' || s == ')' || s == ']') {
                 arr[i] = s;
             }
             i++;
