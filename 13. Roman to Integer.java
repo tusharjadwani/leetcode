503a7f1084999dd51124be98c2c61bb3c6507f35
Tushar Jadwani <117591585+tusharjadwani@users.noreply.github.com>
2026-09-12 18:42:56 +0530

    Updating LeetCode Solution: Roman to Integer

--- 13. Roman to Integer.java
+++ 13. Roman to Integer.java
@@ -1,30 +1,27 @@
 class Solution {
 
-    private int getInt(char roman) {
-        int res = switch (roman) {
-            case 'M' -> 1000;
-            case 'D' -> 500;
-            case 'C' -> 100;
-            case 'L' -> 50;
-            case 'X' -> 10;
-            case 'V' -> 5;
+    private int getNum(char c) {
+
+        return switch (c) {
             case 'I' -> 1;
+            case 'V' -> 5;
+            case 'X' -> 10;
+            case 'L' -> 50;
+            case 'C' -> 100;
+            case 'D' -> 500;
+            case 'M' -> 1000;
             default -> 0;
         };
-        return res;
     }
 
     public int romanToInt(String s) {
-        int res = 0;
-        int max = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            int n = getInt(s.charAt(i));
-            if (n >= max) {
-                res += n;
-                max = n;
-            } else
-                res -= n;
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i + 1 != s.length() && getNum(s.charAt(i + 1)) > getNum(s.charAt(i)))
+                sum -= getNum(s.charAt(i));
+            else
+                sum += getNum(s.charAt(i));
         }
-        return res;
+        return sum;
     }
 }
