c646fa0aa497fb3b7bafbc62efdbb5f2f4f9154f
Tushar Jadwani <117591585+tusharjadwani@users.noreply.github.com>
2026-09-12 17:25:05 +0530

    Updating LeetCode Solution: Contains Duplicate II

--- 219. Contains Duplicate II.java
+++ 219. Contains Duplicate II.java
@@ -3,10 +3,10 @@ class Solution {
         Set<Integer> set = new HashSet<>(k);
 
         for (int i = 0; i < nums.length; i++) {
-            if (set.contains(nums[i]))
+            if (!set.add(nums[i]))
                 return true;
 
-            set.add(nums[i]);
+            // set.add(nums[i]);
 
             if (set.size() > k)
                 set.remove(nums[i - k]);
