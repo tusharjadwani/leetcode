335bd4e9947240c202c15bce89856bd0811fbf01
Tushar Jadwani <117591585+tusharjadwani@users.noreply.github.com>
2026-09-12 12:36:06 +0530

    Updating LeetCode Solution: Contains Duplicate

--- 217. Contains Duplicate.java
+++ 217. Contains Duplicate.java
@@ -3,12 +3,11 @@ class Solution {
         Set<Integer> set = new HashSet<>();
 
         for (int i = 0; i < nums.length; i++) {
-            if (set.contains(nums[i]))
+            if (set.contains(nums[i])) {
                 return true;
-
+            }
             set.add(nums[i]);
         }
-
         return false;
     }
 }
