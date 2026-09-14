846ef4bc686e2325624f04f3aa992181f1507e6b
Tushar Jadwani <117591585+tusharjadwani@users.noreply.github.com>
2026-09-14 16:36:15 +0530

    Updating LeetCode Solution: Majority Element

--- 169. Majority Element.java
+++ 169. Majority Element.java
@@ -1,7 +1,21 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        int count = 0;
+        int ans = nums[0];
 
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        for (int num : nums) {
+
+            if (count == 0)
+                ans = num;
+
+            if (ans == num)
+                count++;
+            else
+                count--;
+
+        }
+
+        return ans;
+
     }
 }
