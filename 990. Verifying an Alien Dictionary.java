ccf585ee4155076c5730b87f246b0a0a885ea547
Tushar Jadwani <117591585+tusharjadwani@users.noreply.github.com>
2026-09-15 18:49:13 +0530

    Updating LeetCode Solution: Verifying an Alien Dictionary

--- 990. Verifying an Alien Dictionary.java
+++ 990. Verifying an Alien Dictionary.java
@@ -1,16 +1,16 @@
 class Solution {
 
-    private Map<Character, Integer> map = new HashMap<>();
+    private int[] arr = new int[26];
 
     private int getNum(char c) {
-        return map.get(c);
+        return arr[c-'a'];
     }
 
     public boolean isAlienSorted(String[] words, String order) {
 
         int i1 = 0;
         for (char c : order.toCharArray()) {
-            map.put(c, i1);
+            arr[c-'a']=i1;
             i1++;
         }
 
