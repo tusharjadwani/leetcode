bb85a5b0ff3b56230a353668e16fd6f0b3cef81b
Tushar Jadwani <117591585+tusharjadwani@users.noreply.github.com>
2026-09-17 18:16:56 +0530

    Updating LeetCode Solution: Group Anagrams

--- 49. Group Anagrams.java
+++ 49. Group Anagrams.java
@@ -1,24 +1,5 @@
 class Solution {
 
-    private boolean isAnagrams(String s1, String s2) {
-
-        if (s1.length() != s2.length())
-            return false;
-
-        int[] chars = new int[26];
-
-        for (int i = 0; i < s1.length(); i++) {
-            chars[s1.charAt(i) - 'a']++;
-            chars[s2.charAt(i) - 'a']--;
-        }
-
-        for (int i = 0; i < chars.length; i++) {
-            if (chars[i] != 0)
-                return false;
-        }
-        return true;
-    }
-
     private String getAnagramKey(String s1) {
 
         int[] chars = new int[26];
@@ -30,7 +11,6 @@ class Solution {
         for (int i = 0; i < chars.length; i++)
             sb.append(chars[i]).append("#");
 
-        System.out.println(sb.toString());
         return sb.toString();
     }
 
@@ -39,7 +19,6 @@ class Solution {
         Map<String, List<String>> map = new HashMap<>();
 
         for (int i = 0; i < strs.length; i++) {
-            System.out.println(strs[i]);
             String st = getAnagramKey(strs[i]);
             if (map.get(st) == null)
                 map.put(st, new ArrayList<>());
