c17022aebbb160b56606c6e74f45fab88ee28842
Tushar Jadwani <117591585+tusharjadwani@users.noreply.github.com>
2026-09-23 18:45:59 +0530

    Adding LeetCode Solution: Longest Consecutive Sequence

---
+++ 128. Longest Consecutive Sequence.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+
+        if (nums.length == 0)
+            return 0;
+
+        Arrays.sort(nums);
+        int count = 1, max = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            if (nums[i + 1] == nums[i])
+                continue;
+
+            if (nums[i + 1] - 1 == nums[i])
+                count++;
+            else {
+                max = Math.max(max, count);
+                count = 1;
+            }
+        }
+        max = Math.max(max, count);
+
+        return max;
+    }
+}
